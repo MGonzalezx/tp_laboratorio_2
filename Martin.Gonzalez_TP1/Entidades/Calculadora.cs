1fecf5d593e755d3e284e82d8a2fe311bc307d3e
MGonzalezx <martingonzalezt97@gmail.com>
2019-09-02 15:47:18 -0300

    tp1

---
+++ Martin.Gonzalez_TP1/Entidades/Calculadora.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public static class Calculadora
+    {
+        #region Metodos
+        private static string ValidarOperador(string operador)
+        {
+            if (String.Equals(operador, "+") || String.Equals(operador, "-") || String.Equals(operador, "/") || String.Equals(operador, "*"))
+            {
+                return operador;
+            }
+            else
+            {
+                return "+";
+            }
+        }
+
+        public static double Operar(Numero num1, Numero num2, string operador)
+        {
+            string operadorAuxiliar = Calculadora.ValidarOperador(operador);
+            double retorno = 0;
+
+            switch (operadorAuxiliar)
+            {
+                case "+":
+                    retorno = num1 + num2;
+                    break;
+                case "-":
+                    retorno = num1 - num2;
+                    break;
+                case "/":
+                    if (string.Equals(num2, 0))
+                    {
+                        retorno = Double.MinValue;
+                    }
+                    else
+                    {
+                        retorno = num1 / num2;
+                    }
+                    break;
+                case "*":
+                    retorno = num1 * num2;
+                    break;
+            }
+            return retorno;
+
+        }
+        #endregion
+    }
+}
---
+++ Martin.Gonzalez_TP1/Entidades/Numero.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Numero
+    {
+        #region Variables
+        private double numero;
+        #endregion
+
+        #region Constructores
+        public Numero(): this(0)
+        {
+            
+        }
+        public Numero(double numero)
+        {
+            this.numero = numero;
+        }
+        public Numero(string strNumero)
+        {
+            this.SetNumero = strNumero;
+        }
+        #endregion
+
+        #region Metodos
+
+        private string SetNumero
+        {
+            set { numero = this.ValidarNumero(value); }
+        }
+
+        private double ValidarNumero(string strNumero)
+        {
+            double auxiliar;
+
+            if(!double.TryParse(strNumero, out auxiliar))
+            {
+                auxiliar = 0;
+            }
+            return auxiliar;
+        }
+
+        public string BinarioDecimal(string binario)
+        {
+            string retorno = "Valor inválido";
+            bool flag = true;
+            int numeroAuxiliar;
+            for (int i = 0; i < binario.Length; i++)
+            {
+                if (binario[i] != 1 || binario[i] != 0)
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag)
+            {
+                numeroAuxiliar = Convert.ToInt32(binario, 10);
+                retorno = numeroAuxiliar.ToString();
+            }
+            return retorno;
+        }
+
+        public static string DecimalBinario(double numero)
+        {
+            string retorno;
+            int auxiliar;
+            auxiliar = Convert.ToInt32(Math.Abs(numero));
+            retorno = Convert.ToString(auxiliar, 2);
+            return retorno;
+        }
+
+        public string DecimalBinario(string numero)
+        {
+            string retorno = "Valor invalido";
+            double numeroAuxiliar;
+            if (!Double.TryParse(numero, out numeroAuxiliar))
+            {
+                retorno = Numero.DecimalBinario(numeroAuxiliar);
+            }
+            return retorno;
+        }
+
+        #endregion
+
+        #region Operadores
+        public static double operator +(Numero n1, Numero n2)
+        {
+            return n1.numero + n2.numero;
+        }
+
+        public static double operator -(Numero n1, Numero n2)
+        {
+            return n1.numero - n2.numero;
+        }
+
+        public static double operator *(Numero n1, Numero n2)
+        {
+            return n1.numero * n2.numero;
+        }
+        public static double operator /(Numero n1, Numero n2)
+        {
+            if(n2.numero != 0)
+            {
+                return n1.numero / n2.numero;
+            }else
+            {
+                return double.MinValue;
+            }
+                        
+        }
+        #endregion
+
+    }
+}
