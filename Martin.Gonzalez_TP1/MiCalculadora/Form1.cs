f76686a446e3886a824c9dc699b550b44ca3a3f9
MGonzalezx <martingonzalezt97@gmail.com>
2019-09-13 13:19:09 -0300

    TP_1

---
+++ Martin.Gonzalez_TP1/MiCalculadora/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+
+namespace MiCalculadora
+{
+    public partial class FormCalculadora : Form
+    {
+        public FormCalculadora()
+        {
+            InitializeComponent();
+            cmbOperador.Items.Add("+");
+            cmbOperador.Items.Add("-");
+            cmbOperador.Items.Add("*");
+            cmbOperador.Items.Add("/");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private static double Operar(string numero1, string numero2, string operador)
+        {
+            Numero numeroUno = new Numero(numero1);
+            Numero numeroDos = new Numero(numero2);
+            double resultado = Calculadora.Operar(numeroUno, numeroDos, operador);
+            return resultado;
+        }
+
+        private void Limpiar()
+        {
+            txtNumero1.Text = string.Empty;
+            txtNumero2.Text = string.Empty;
+            cmbOperador.Text = string.Empty;
+            lblResultado.Text = string.Empty;
+        }
+
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            double calcular = FormCalculadora.Operar(txtNumero1.Text, txtNumero2.Text, cmbOperador.Text);
+            lblResultado.Text = calcular.ToString();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            this.Limpiar();
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnConvertirABinario_Click(object sender, EventArgs e)
+        {
+            double calcular = FormCalculadora.Operar(txtNumero1.Text, txtNumero2.Text, cmbOperador.Text);
+            lblResultado.Text = Numero.DecimalBinario(calcular);
+        }
+
+        private void btnConvertirADecima_Click(object sender, EventArgs e)
+        {
+            double calcular = FormCalculadora.Operar(txtNumero1.Text, txtNumero2.Text, cmbOperador.Text);
+            lblResultado.Text = calcular.ToString();
+        }
+    }
+}
